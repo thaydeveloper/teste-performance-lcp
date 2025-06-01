2279165c0f944d429d2ed51793c97764440a01cc
thaydeveloper <thaydeveloper26@gmail.com>
2025-06-01 12:57:52 -0300

    fix: fix na imagem

--- src/app/(home)/sub-components/Hero/Hero.tsx
+++ src/app/(home)/sub-components/Hero/Hero.tsx
@@ -25,14 +25,23 @@ const Hero: FC<HeroProps> = ({ description, buttonText }) => {
       <Image
         src={images.backgroundHome}
         alt="Pessoa usando aplicativo de eSIM no celular"
-        width={1920}
-        height={1080}
+        width={1728}
+        height={625}
         priority={true}
         quality={70}
         fetchPriority="high"
-        decoding="auto"
+        decoding="async"
         loading="eager"
         sizes="100vw"
+        style={{
+          objectFit: 'cover',
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: '100%',
+          zIndex: -1,
+        }}
       />
       <div className="w-full max-w-[791px] h-auto absolute pt-[50px] sm:top-[290px] left-1/2 transform -translate-x-1/2 rounded-[50px] flex flex-col justify-center items-center text-center text-white ">
         <div className="w-full h-auto rounded-[50px] py-0 flex flex-col justify-center items-center">
